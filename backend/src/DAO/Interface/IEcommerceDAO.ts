5f98226351b99b0d436cf5d424222ff368185bda
Guilherme Carvalho <48804617+guilhermepdc@users.noreply.github.com>
2020-11-27 22:08:16 -0300

    Delete IEcommerceDAO.ts

--- backend/src/DAO/Interface/IEcommerceDAO.ts
+++
@@ -1,8 +0,0 @@
-import Product from '../../models/Product';
-export default interface IProductDao {
-    save(arg0: Product): Promise<Product>;
-    getAll(): Promise<Product>;
-    getById(id: number): Promise<Product>;
-    update(oldObj: Product, newObj: Product): void;
-    delete(arg0: Product): void;
-}
